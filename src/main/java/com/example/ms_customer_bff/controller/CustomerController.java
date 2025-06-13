5e8b7a56d319fbf3b1b86bcee619ffed2bff1d33
Lucas Soto <lucassoto3333@gmail.com>
2025-06-13 03:38:10 -0400

    se agrego  origin en controller

--- src/main/java/com/example/ms_customer_bff/controller/CustomerController.java
+++ src/main/java/com/example/ms_customer_bff/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.ms_customer_bff.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import com.example.ms_customer_bff.model.dto.CustomerDTO;
 import com.example.ms_customer_bff.service.CustomerService;
 
 @RestController
+@CrossOrigin("*")
 public class CustomerController {
 
     @Autowired
